0abdf5ed827615394449430e640775eb5e33caf0
MDDPAEEGproject <andreyilse@yandex.ru>
2017-07-03 20:40:02 +0300

    Initial Commit

---
+++ weathere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weathere
+//
+//  Created by Андрей Илалов on 03.07.17.
+//  Copyright © 2017 Андрей Илалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
